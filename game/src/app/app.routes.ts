00c97c91cb40e9dcaa37c3dc748aad57e9dddf39
m.bugai <m.bugai@plarium.com>
2016-08-05 11:13:16 +0300

    remove about and details screens

--- game/src/app/app.routes.ts
+++ game/src/app/app.routes.ts
@@ -10,17 +10,6 @@ export const routes: RouterConfig = [
   { path: '',      component: Home },
   { path: 'home',  component: Home },
   { path: 'game',  component: Game },
-  // make sure you match the component type string to the require in asyncRoutes
-  { path: 'about', component: 'About',
-    resolve: {
-      'yourData': DataResolver
-    }},
-  // async components with children routes must use WebpackAsyncRoute
-  { path: 'detail', component: 'Detail',
-    canActivate: [ WebpackAsyncRoute ],
-    children: [
-      { path: '', component: 'Index' }  // must be included
-    ]},
   { path: '**',    component: NoContent },
 ];
 
@@ -31,17 +20,12 @@ export const routes: RouterConfig = [
 export const asyncRoutes: AsyncRoutes = {
   // we have to use the alternative syntax for es6-promise-loader to grab the routes
   // 'Game': require('es6-promise-loader!./game'),
-  'About': require('es6-promise-loader!./about'),
-  'Detail': require('es6-promise-loader!./+detail'),
-  'Index': require('es6-promise-loader!./+detail'), // must be exported with detail/index.ts
 };
 
 
 // Optimizations for initial loads
 // An array of callbacks to be invoked after bootstrap to prefetch async routes
 export const prefetchRouteCallbacks: Array<IdleCallbacks> = [
-  asyncRoutes['About'],
-  asyncRoutes['Detail'],
   // asyncRoutes['Game'],
    // es6-promise-loader returns a function
 ];
