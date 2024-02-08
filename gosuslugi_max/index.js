fe446fcb671d1af5cdb22a7367764bf685b52cb8
m.bugai <m.bugai@plarium.com>
2024-02-08 10:14:17 +0200

    wip

--- gosuslugi_max/index.js
+++ gosuslugi_max/index.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.use(express.static('public'))
 
-app.listen(port, '81.200.155.19', () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, '81.200.155.19')
